2c5a3f82a01eb6c63c4812a1840b1b59eac2cdf9
ahnafwafiq <ahnafwafiq69@gmail.com>
2023-09-01 23:12:21 +0600

    Added Todo Editing functionality

--- src/components/TodoList/TodoItem.tsx
+++ src/components/TodoList/TodoItem.tsx
@@ -9,15 +9,23 @@ import {
     updateDoc,
 } from "firebase/firestore";
 import app from "../../Firebase-config";
+import { useRef, useState } from "react";
+import { AiOutlineSave } from "react-icons/ai";
 
 interface Props {
     finished: boolean;
     children: string;
     todoId: string;
-    updateTodoList: (todoId: string, mode: "delete" | "finished") => void;
+    updateTodoList: (
+        todoId: string,
+        mode: "delete" | "finished" | "edit",
+        newTodo?: string,
+    ) => void;
 }
 
 function TodoItem({ children, finished, todoId, updateTodoList }: Props) {
+    const [isEditing, setEditing] = useState<boolean>(false);
+    const editRef = useRef<HTMLInputElement>(null);
     const iconSize = "24px";
     return (
         <tr className={Styles.todoItem}>
@@ -34,10 +42,10 @@ function TodoItem({ children, finished, todoId, updateTodoList }: Props) {
                         className={Styles.checkBox}
                         type="checkbox"
                         onChange={(e) => {
+                            setEditing(false);
                             if (finished) return;
                             if (e.target.checked) {
                                 updateTodoList(todoId, "finished");
-                                // updateTodoList(todoId, "finished");
                                 const ref = doc(
                                     collection(getFirestore(app), "todos"),
                                     todoId,
@@ -50,18 +58,56 @@ function TodoItem({ children, finished, todoId, updateTodoList }: Props) {
                     />
                 )}
             </td>
-            <td
-                className={
-                    finished
-                        ? [Styles.todoText, Styles.textFinished].join(" ")
-                        : [Styles.todoText, "todoText"].join(" ")
-                }
-            >
-                {children}
-            </td>
+            {isEditing ? (
+                <td>
+                    <input
+                        type="text"
+                        ref={editRef}
+                        placeholder={children}
+                        className={Styles.editTodo}
+                    />
+                </td>
+            ) : (
+                <td
+                    className={
+                        finished
+                            ? [Styles.todoText, Styles.textFinished].join(" ")
+                            : Styles.todoText
+                    }
+                >
+                    {children}
+                </td>
+            )}
             <td>
-                {finished ? null : (
-                    <div className={Styles.editButton} title="Edit Todo">
+                {finished ? null : isEditing ? (
+                    <div
+                        className={Styles.editButton}
+                        onClick={() => {
+                            const newTodo = editRef?.current?.value;
+                            if (!newTodo) return;
+                            updateTodoList(todoId, "edit", newTodo);
+                            setEditing(false);
+                            const ref = doc(
+                                collection(getFirestore(app), "todos"),
+                                todoId,
+                            );
+                            updateDoc(ref, {
+                                name: newTodo,
+                            });
+                            updateTodoList(todoId, "edit", newTodo);
+                        }}
+                    >
+                        <AiOutlineSave size={iconSize} />
+                    </div>
+                ) : (
+                    <div
+                        className={Styles.editButton}
+                        title="Edit Todo"
+                        onClick={() => {
+                            setEditing(() => true);
+                            editRef.current?.focus();
+                        }}
+                    >
                         <BiEditAlt size={iconSize} />
                     </div>
                 )}
@@ -71,6 +117,7 @@ function TodoItem({ children, finished, todoId, updateTodoList }: Props) {
                     className={Styles.deleteButton}
                     title="Delete Todo"
                     onClick={() => {
+                        setEditing(false);
                         updateTodoList(todoId, "delete");
                         const todosRef = collection(getFirestore(app), "todos");
                         deleteDoc(doc(todosRef, todoId));
